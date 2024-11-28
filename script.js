b609012d3b5f9c5005b68cbbaff8704708c43db0
Somesh Pratap Singh <146454603+sammy0318@users.noreply.github.com>
2024-11-28 20:29:52 +0530

    Update script.js
    yaayaa

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ document.getElementById("download-resume").addEventListener("click", function (e
     link.click(); // Simulate a click
     document.body.removeChild(link); // Remove the link after download
 });
+
+document.getElementById("download-resume").addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent default anchor behavior
+    const resumeUrl = "Somesh Pratap Singh Resume.pdf"; // Update with the correct path to your resume
+    const link = document.createElement("a");
+    link.href = resumeUrl;
+    link.download = "Somesh_Pratap_Singh_Resume.pdf"; // The downloaded file name
+    document.body.appendChild(link); // Append the link to the document
+    link.click(); // Simulate a click
+    document.body.removeChild(link); // Remove the link after download
+});
+
+
+const toggleTheme = () => {
+    document.body.classList.toggle('light-mode');
+};
+
